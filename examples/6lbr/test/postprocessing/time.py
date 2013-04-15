dca082d9181ab30beb7a357952f2a86f3f0e0aaf
Mathieu Ocaña <mathieu.ocana@cetic.be>
2013-04-15 17:35:57 +0200

    Add computation of 'move mote' time delta in the post-processing lib

--- examples/6lbr/test/postprocessing/time.py
+++ examples/6lbr/test/postprocessing/time.py
@@ -46,6 +46,9 @@ time_delta["mote-detect"] = time_relative["Mote detected"] - time_relative["Mote
 
 if time_relative.has_key("Mote ping"):
     time_delta["ping1"] = time_relative["Mote reached"] - time_relative["Mote ping"]
+elif time_relative.has_key("Moved mote"):
+    time_delta["ping1"] = time_relative["Mote reached"] - time_relative["Mote ping"]
+    time_delta["pingm"] = time_relative["Move mote reached"] - time_relative["Move mote ping"]
 else:
     time_delta["ping1"] = time_relative["Mote reached"] - time_relative["Mote ping"]
     time_delta["ping2"] = time_relative["Mote reached2"] - time_relative["Mote ping2"]
