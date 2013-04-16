2a6697e6baaac5d97f4e30cba9b12b248e1a726e
Sébastien Dawans <sebastien.dawans@cetic.be>
2013-04-16 12:39:55 +0200

    Changed time.py into importable module

--- examples/6lbr/test/postprocessing/time.py
+++ examples/6lbr/test/postprocessing/time.py
@@ -19,35 +19,42 @@ Stop Test = 29759.605885
 import os
 import sys
 
-if len(argv)<2:
-    exit(1)
 
-infile = argv[1]
-if not os.path.exists(infile):
-    exit(1)
+def parse_times(infile):
 
-time_relative = {}
-time_delta = {}
+    if not os.path.exists(infile):
+        exit(1)
+
+    time_relative = {}
+    time_delta = {}
     
-with open(infile) as f:
-    for line in f:
-        if line.find(" = ") != -1:
-            rawinfo = line.split(" = ")
-            time_relative[rawinfo[0]] = float(rawinfo[1])
+    with open(infile) as f:
+        for line in f:
+            if line.find(" = ") != -1:
+                rawinfo = line.split(" = ")
+                time_relative[rawinfo[0]] = float(rawinfo[1])
 
-time_delta["test"] = time_relative["Stop Test"]
-time_delta["network-uptime"] = time_relative["Network stopped"] - time_relative["Network start"]
-time_delta["network-start"] = time_relative["Network started"] - time_relative["Network start"]
-time_delta["network-stop"] = time_relative["Network stopped"] - time_relative["Network stop"]
-time_delta["mote-uptime"] = time_relative["Mote stopped"] - time_relative["Mote started"]
-time_delta["mote-start"] = time_relative["Mote detect start"] - time_relative["Mote started"]
-time_delta["mote-stop"] = time_relative["Mote stopped"] - time_relative["Mote reached"]
-time_delta["mote-detect"] = time_relative["Mote detected"] - time_relative["Mote detect start"]
+    time_delta["test"] = time_relative["Stop Test"]
+    time_delta["network-uptime"] = time_relative["Network stopped"] - time_relative["Network start"]
+    time_delta["network-start"] = time_relative["Network started"] - time_relative["Network start"]
+    time_delta["network-stop"] = time_relative["Network stopped"] - time_relative["Network stop"]
+    time_delta["mote-uptime"] = time_relative["Mote stopped"] - time_relative["Mote started"]
+    time_delta["mote-start"] = time_relative["Mote detect start"] - time_relative["Mote started"]
+    time_delta["mote-stop"] = time_relative["Mote stopped"] - time_relative["Mote reached"]
+    time_delta["mote-detect"] = time_relative["Mote detected"] - time_relative["Mote detect start"]
 
-time_delta["ping1"] = time_relative["Mote reached"] - time_relative["Mote ping"]
+    time_delta["ping1"] = time_relative["Mote reached"] - time_relative["Mote ping"]
 
-if time_relative.has_key("Moved mote"):
-    time_delta["pingm"] = time_relative["Move mote reached"] - time_relative["Move mote ping"]
-elif time_relative.has_key("Moved ping2"):
-    time_delta["ping2"] = time_relative["Mote reached2"] - time_relative["Mote ping2"]
-    
+    if time_relative.has_key("Moved mote"):
+        time_delta["pingm"] = time_relative["Move mote reached"] - time_relative["Move mote ping"]
+    elif time_relative.has_key("Moved ping2"):
+        time_delta["ping2"] = time_relative["Mote reached2"] - time_relative["Mote ping2"]
+
+    return time_delta
+
+if __name__ == "__main__":
+    import sys
+    import os
+    if len(argv)<2:
+        exit(1)
+    main(sys.argv[1])
