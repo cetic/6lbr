6f3d00ee4fabde53852f3b040740108fedc9081e
Laurent Deru <laurent.deru@cetic.be>
2013-05-21 23:52:53 +0200

    Allow modification of Router and RPL mode

--- platform/econotag/contiki-conf.h
+++ platform/econotag/contiki-conf.h
@@ -180,8 +180,12 @@ typedef unsigned long rtimer_clock_t;
 #define UIP_CONF_LL_802154              1
 #define UIP_CONF_LLH_LEN                0
 
-#define UIP_CONF_ROUTER                 1  
+#ifndef UIP_CONF_ROUTER
+#define UIP_CONF_ROUTER                 1
+#endif
+#ifndef UIP_CONF_IPV6_RPL
 #define UIP_CONF_IPV6_RPL               1
+#endif
 
 #define UIP_CONF_DS6_NBR_NBU     30
 #define UIP_CONF_DS6_ROUTE_NBU   30
