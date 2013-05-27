90aaf1433642866f43cc96a0201a6971a2a43154
Laurent Deru <laurent.deru@cetic.be>
2013-05-27 22:11:23 +0200

    Rename tests

---
+++ examples/6lbr/test/multi_br.py
@@ -0,0 +1,30 @@
+import base
+from base import skipUnlessTrue, skipUnlessFalse
+
+class MultiBrNonRegressionScenarios(base.TestScenarios):
+    @skipUnlessTrue("S1")
+    def test_S1_move_mote_disjoint(self):
+        """
+        Ping from the computer to the mote when the PC knows the BR but the BR does not know the
+        mote.
+        """
+        self.assertTrue(self.support.start_6lbr(), "Could not start 6LBR")
+        self.set_up_network()
+        self.assertTrue(self.support.start_mote(), "Could not start up mote")
+        self.assertTrue(self.support.wait_mote_in_6lbr(30), "Mote not detected")
+        self.assertTrue(self.support.wait_ping_mote(60), "Mote is not responding")
+        print >> sys.stderr, "Moving mote..."
+        self.support.wsn.move_mote(self.support.test_mote.mote_id, -1)
+        sleep(5)
+        self.assertTrue(self.support.wait_ping_mote(240), "Mote is not responding")
+        self.assertTrue(self.support.stop_mote(), "Could not stop mote")
+        self.tear_down_network()
+        self.assertTrue(self.support.stop_6lbr(), "Could not stop 6LBR")
+
+    @skipUnlessTrue("S2")
+    def test_S2_move_mote_overlap(self):
+        pass
+    
+    @skipUnlessTrue("S3")
+    def test_S3_br_failure(self):
+        pass
