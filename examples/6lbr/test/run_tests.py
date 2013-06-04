212f3f2f9e32743369e670de1f2cfe48516903a0
Laurent Deru <laurent.deru@cetic.be>
2013-06-04 22:36:52 +0200

    Introduce flexible tests runner and rename test packages

---
+++ examples/6lbr/test/run_tests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2.7
+
+import unittest
+import argparse
+import sys
+
+try:
+    import config
+except ImportError:
+    print "Configuration file not found, using default..."
+
+
+parser = argparse.ArgumentParser(description='6LBR tests')
+parser.add_argument('--modes', metavar='<mode name>', dest='modes', nargs='+',
+                   help='Modes to run')
+parser.add_argument('--scenarios', metavar='<test scenarios name>', dest='scenarios',
+                   help='Test scenarios to be executed')
+
+args = parser.parse_args()
+
+test_scenarios_module = __import__(args.scenarios, fromlist=[args.scenarios])
+test_scenarios = getattr(test_scenarios_module, args.scenarios)
+
+config.scenarios=test_scenarios
+
+for mode in args.modes:
+    mode_module=__import__(mode, fromlist=[mode])
+    setattr(sys.modules['__main__'], mode, getattr(mode_module, mode))
+    
+unittest.main(argv=[sys.argv[0]], exit=False, verbosity=1)
