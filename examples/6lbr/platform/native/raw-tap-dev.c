db12e981bd730fa0a61d6d71e488a098e51de506
Laurent Deru <laurent.deru@cetic.be>
2013-09-02 16:23:04 +0200

    Fix raw-tap-dev debug flag

--- examples/6lbr/platform/native/raw-tap-dev.c
+++ examples/6lbr/platform/native/raw-tap-dev.c
@@ -90,7 +90,7 @@ extern int slipfd;
 extern void slip_flushbuf(int fd);
 //End of temporary
 
-#if 1                           //DEBUG
+#if DEBUG
 #define PRINTETHADDR(addr) printf(" %02x:%02x:%02x:%02x:%02x:%02x ",(*addr)[0], (*addr)[1], (*addr)[2], (*addr)[3], (*addr)[4], (*addr)[5])
 #else
 #define PRINTETHADDR(addr)
