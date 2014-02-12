07058c86997eb665d877a863cf93297b8507e29a
Laurent Deru <laurent.deru@cetic.be>
2014-02-12 13:17:29 +0100

    Add watchdog for Nooliberry

--- platform/nooliberry/contiki-nooliberry-main.c
+++ platform/nooliberry/contiki-nooliberry-main.c
@@ -109,6 +109,9 @@ init_lowlevel(void)
   uint8_t i;
   unsigned char ds2411_id[6];
 
+  watchdog_init();
+  watchdog_start();
+
   /* Second rs232 port for debugging */
   rs232_init(RS232_PORT_1, USART_BAUD_38400,
              USART_PARITY_NONE | USART_STOP_BITS_1 | USART_DATA_BITS_8);
@@ -228,6 +231,7 @@ main(void)
   /* Main scheduler loop */
   while(1) {
     process_run();
+    watchdog_periodic();
   }
 
   return 0;
