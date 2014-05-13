f70474531a642a12ed57201ed2b810ac17e08384
Sébastien De Fauw <sdefauw@Laptop-Seb.local>
2014-05-13 10:43:47 +0200

    test multi process simulation
    Don't work !!!

--- examples/6lowpan-nd/test-quantitative/topology/sim.sh
+++ examples/6lowpan-nd/test-quantitative/topology/sim.sh
@@ -14,6 +14,46 @@ do
 	cat $TYPE/temp.testlog >> $TYPE/$OUTFILE
 done;
 
+
+# #----------------------- MULTI PROCESSUS ---
+# I=0
+# for FILENAME in $TYPE/*.csc
+# do
+# 	rm -rf $TYPE/temp$I
+# 	mkdir $TYPE/temp$I
+# 	cp $FILENAME $TYPE/temp$I/
+# 	I=$(($I+1))
+# done;
+
+# I=0
+# PIDMAX=0
+# PIDMIN=99999999
+# for FILENAME in $TYPE/temp*/*.csc
+# do
+# 	echo "--- $FILENAME ---"
+# 	sh $CONTIKI/regression-tests/simexec.sh "true" "$FILENAME" "$CONTIKI" "$TYPE/temp$I/out" "1" &
+# 	PIDM=$!
+# 	PIDMAX=$(($PIDM>$PIDMAX?$PIDM:$PIDMAX))
+# 	PIDMIN=$(($PIDM<$PIDMIN?$PIDM:$PIDMIN))
+# 	I=$(($I+1))
+# done;
+
+# # Wait until all done
+# for ((j=$PIDMIN; j<=$PIDMAX; j++)); do
+# 	echo $j
+# 	wait $j
+# done
+
+# #Merge file
+# rm $TYPE/$OUTFILE
+# I=0
+# for FILENAME in $TYPE/temp*/*.csc
+# do
+# 	echo "------ $FILENAME ------" >> $TYPE/$OUTFILE
+# 	cat "$TYPE/temp$I/out.testlog" >> $TYPE/$OUTFILE
+# 	I=$(($I+1))
+# done;
+
 # #----------------------- MULTI PROCESSUS --- /!\ DON'T WORK !!!
 # # execute simulation
 # I=0
@@ -22,12 +62,12 @@ done;
 # for FILENAME in $TYPE/*.csc
 # do
 # 	echo "--- $FILENAME ---"
-# 	java -jar $CONTIKI/tools/cooja/dist/cooja.jar -nogui=$FILENAME -contiki=$CONTIKI -random-seed=1 > $TYPE/temp$I.log &
+# 	java -Xshare:on -jar $CONTIKI/tools/cooja/dist/cooja.jar -nogui=$FILENAME -contiki=$CONTIKI -random-seed=1 > $TYPE/temp$I.log &
 # 	#sh $CONTIKI/regression-tests/simexec.sh "true" "$FILENAME" "$CONTIKI" "$TYPE/temp$I" "1" &
 # 	PIDM=$!
 # 	PIDMAX=$(($PIDM>$PIDMAX?$PIDM:$PIDMAX))
 # 	PIDMIN=$(($PIDM<$PIDMIN?$PIDM:$PIDMIN))
-# 	cat $TYPE/temp.testlog >> $TYPE/$OUTFILE
+# 	cat $TYPE/temp$I.testlog >> $TYPE/$OUTFILE
 # 	I=$(($I+1))
 # done;
 
@@ -48,4 +88,4 @@ done;
 # done;
 
 
-echo "--------- END -------------"
+# echo "--------- END -------------"
