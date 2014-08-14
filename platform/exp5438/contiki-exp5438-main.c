529013b240f85d6f91585496af521186015a373b
Laurent Deru <laurent.deru@cetic.be>
2014-08-14 16:18:39 +0200

    Allow deactivation of the ipv6 global static address

--- platform/exp5438/contiki-exp5438-main.c
+++ platform/exp5438/contiki-exp5438-main.c
@@ -254,6 +254,7 @@ main(int argc, char **argv)
     printf("%02x%02x\n", lladdr->ipaddr.u8[14], lladdr->ipaddr.u8[15]);
   }
 
+#if !UIP_DS6_NO_STATIC_ADDRESS
   if(!UIP_CONF_IPV6_RPL) {
     uip_ipaddr_t ipaddr;
     int i;
@@ -268,6 +269,7 @@ main(int argc, char **argv)
     printf("%02x%02x\n",
            ipaddr.u8[7 * 2], ipaddr.u8[7 * 2 + 1]);
   }
+#endif /* !UIP_DS6_NO_STATIC_ADDRESS */
 
 #else /* WITH_UIP6 */
 
