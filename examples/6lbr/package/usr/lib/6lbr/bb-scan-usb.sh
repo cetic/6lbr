5fac41fa1b924623db0cea4836d22af0993257c0
Laurent Deru <laurent.deru@cetic.be>
2015-05-06 06:59:07 +0000

    Add usb scanning workaround for BB

---
+++ examples/6lbr/package/usr/lib/6lbr/bb-scan-usb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Reset and scan USB devices..."
+
+#Rebind USB controller
+echo "usb1" > /sys/bus/usb/drivers/usb/unbind
+sleep 1
+echo "usb1" > /sys/bus/usb/drivers/usb/bind
+sleep 1
+
+#Disable auto-suspend
+echo "on" > /sys/bus/usb/devices/usb1/power/control
+
+#Force USB controller scan
+cat /dev/bus/usb/001/001 > /dev/null
