e6d7f7a53683a44f1f4586f4c892f1b2c5a89eb3
Laurent Deru <laurent.deru@cetic.be>
2015-09-22 16:49:27 +0200

    Update OpenMote's startup-gcc.c with latest modifications

--- platform/openmote/startup-gcc.c
+++ platform/openmote/startup-gcc.c
@@ -40,6 +40,7 @@
 #include "reg.h"
 #include "flash-cca.h"
 #include "sys-ctrl.h"
+#include "rom-util.h"
 #include "uart.h"
 
 #include <stdint.h>
@@ -62,6 +63,7 @@ void cc2538_rf_rx_tx_isr(void);
 void cc2538_rf_err_isr(void);
 void udma_isr(void);
 void udma_err_isr(void);
+void crypto_isr(void);
 
 /* Boot Loader Backdoor selection */
 #if FLASH_CCA_CONF_BOOTLDR_BACKDOOR
@@ -101,7 +103,7 @@ void uart1_isr(void);
 #endif /* UART_CONF_ENABLE */
 /*---------------------------------------------------------------------------*/
 /* Allocate stack space */
-static unsigned long stack[512];
+static unsigned long stack[512] __attribute__ ((section(".stack")));
 /*---------------------------------------------------------------------------*/
 /* Linker construct indicating .text section location */
 extern uint8_t _text[0];
@@ -280,18 +282,18 @@ void(*const vectors[])(void) =
   usb_isr,                    /* 156 USB */
   cc2538_rf_rx_tx_isr,        /* 157 RFCORE RX/TX */
   cc2538_rf_err_isr,          /* 158 RFCORE Error */
-  default_handler,            /* 159 AES */
+  crypto_isr,                 /* 159 AES */
   default_handler,            /* 160 PKA */
   rtimer_isr,                 /* 161 SM Timer */
   default_handler,            /* 162 MACTimer */
 };
 /*---------------------------------------------------------------------------*/
 /* Linker constructs indicating .data and .bss segment locations */
-extern unsigned long _etext;
-extern unsigned long _data;
-extern unsigned long _edata;
-extern unsigned long _bss;
-extern unsigned long _ebss;
+extern uint8_t _ldata;
+extern uint8_t _data;
+extern uint8_t _edata;
+extern uint8_t _bss;
+extern uint8_t _ebss;
 /*---------------------------------------------------------------------------*/
 /* Weak interrupt handlers. */
 void
@@ -310,26 +312,13 @@ default_handler(void)
 void
 reset_handler(void)
 {
-  unsigned long *pul_src, *pul_dst;
-
   REG(SYS_CTRL_EMUOVR) = 0xFF;
 
   /* Copy the data segment initializers from flash to SRAM. */
-  pul_src = &_etext;
-
-  for(pul_dst = &_data; pul_dst < &_edata;) {
-    *pul_dst++ = *pul_src++;
-  }
+  rom_util_memcpy(&_data, &_ldata, &_edata - &_data);
 
   /* Zero-fill the bss segment. */
-  __asm("    ldr     r0, =_bss\n"
-        "    ldr     r1, =_ebss\n"
-        "    mov     r2, #0\n"
-        "    .thumb_func\n"
-        "zero_loop:\n"
-        "        cmp     r0, r1\n"
-        "        it      lt\n"
-        "        strlt   r2, [r0], #4\n" "        blt     zero_loop");
+  rom_util_memset(&_bss, 0, &_ebss - &_bss);
 
   /* call the application's entry point. */
   main();
