476fb9d1f24e3b7fd73001d4dc1f413ab12b4a77
Simon Duquennoy <simon.duquennoy@inria.fr>
2016-03-10 11:34:23 +0100

    uip6: restore proto after calling remove_ext_hdr

--- core/net/ipv6/uip6.c
+++ core/net/ipv6/uip6.c
@@ -1487,6 +1487,7 @@ uip_process(uint8_t flag)
  udp_input:
 
   remove_ext_hdr();
+  UIP_IP_BUF->proto = UIP_PROTO_UDP;
 
   PRINTF("Receiving UDP packet\n");
  
@@ -1607,6 +1608,7 @@ uip_process(uint8_t flag)
  tcp_input:
 
   remove_ext_hdr();
+  UIP_IP_BUF->proto = UIP_PROTO_TCP;
 
   UIP_STAT(++uip_stat.tcp.recv);
   PRINTF("Receiving TCP packet\n");
