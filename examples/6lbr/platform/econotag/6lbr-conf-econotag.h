4713ba17cf66cf2a5b5947a646d7e5770b6989f1
Laurent Deru <laurent.deru@cetic.be>
2016-04-27 17:15:34 +0200

    Remove DNS Proxy from Redwire IO

--- examples/6lbr/platform/econotag/6lbr-conf-econotag.h
+++ examples/6lbr/platform/econotag/6lbr-conf-econotag.h
@@ -60,10 +60,10 @@
 
 #else
 #undef UIP_CONF_DS6_NBR_NBU
-#define UIP_CONF_DS6_NBR_NBU     40
+#define UIP_CONF_DS6_NBR_NBU     50
 
 #undef UIP_CONF_MAX_ROUTES
-#define UIP_CONF_MAX_ROUTES   40
+#define UIP_CONF_MAX_ROUTES   50
 
 #endif
 
