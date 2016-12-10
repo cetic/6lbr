2e9c459fbd808df6db567f0fc90a3a504ad96835
Yasuyuki Tanaka <yasuyuki.tanaka@inf.ethz.ch>
2016-12-10 03:20:42 +0100

    NUD: adopt uip_ds6_nbr_update_lladdr()
    nbr_table_update_lladdr() fails to changing the link-layer address of a
    neighbor cache when a new link-layer address is already in nbr-table.
    
    uip_ds6_nbr_update_lladdr() can handles such a situation. It changes the
    link-layer address even if the new link-layer address is in nbr-table as
    long as there is no neighbor cache associated wit the new address.

--- core/net/ipv6/uip-nd6.c
+++ core/net/ipv6/uip-nd6.c
@@ -236,9 +236,8 @@ ns_input(void)
           if(lladdr == NULL) {
             goto discard;
           }
-          if(memcmp(&nd6_opt_llao[UIP_ND6_OPT_DATA_OFFSET],
-              lladdr, UIP_LLADDR_LEN) != 0) {
-            if(nbr_table_update_lladdr((const linkaddr_t *)lladdr, (const linkaddr_t *)&lladdr_aligned, 1) == 0) {
+          if(memcmp(&lladdr_aligned, lladdr, UIP_LLADDR_LEN) != 0) {
+            if(uip_ds6_nbr_update_lladdr(&nbr, &lladdr_aligned) == 0) {
               /* failed to update the lladdr */
               goto discard;
             }
@@ -580,20 +579,19 @@ na_input(void)
       if(nd6_opt_llao == NULL || !extract_lladdr_from_llao_aligned(&lladdr_aligned)) {
         goto discard;
       }
-      if(nbr_table_update_lladdr((const linkaddr_t *)lladdr, (const linkaddr_t *)&lladdr_aligned, 1) == 0) {
+      if(uip_ds6_nbr_update_lladdr(&nbr, &lladdr_aligned) == 0) {
         /* failed to update the lladdr */
         goto discard;
       }
 
-      if(is_solicited) {
+      if(!is_solicited) {
+        nbr->state = NBR_STALE;
+      } else {
         nbr->state = NBR_REACHABLE;
         nbr->nscount = 0;
 
         /* reachable time is stored in ms */
         stimer_set(&(nbr->reachable), uip_ds6_if.reachable_time / 1000);
-
-      } else {
-        nbr->state = NBR_STALE;
       }
       nbr->isrouter = is_router;
     } else { /* NBR is not INCOMPLETE */
@@ -610,7 +608,7 @@ na_input(void)
         if(is_override || !is_llchange || nd6_opt_llao == NULL) {
           if(nd6_opt_llao != NULL && is_llchange) {
             if(!extract_lladdr_from_llao_aligned(&lladdr_aligned) ||
-               nbr_table_update_lladdr((const linkaddr_t *) lladdr, (const linkaddr_t *) &lladdr_aligned, 1) == 0) {
+               uip_ds6_nbr_update_lladdr(&nbr, &lladdr_aligned) == 0) {
               /* failed to update the lladdr */
               goto discard;
             }
@@ -1042,7 +1040,8 @@ ra_input(void)
         if(memcmp(&nd6_opt_llao[UIP_ND6_OPT_DATA_OFFSET],
                   lladdr, UIP_LLADDR_LEN) != 0) {
           /* change of link layer address */
-          if(nbr_table_update_lladdr((const linkaddr_t *)lladdr, (const linkaddr_t *)&lladdr_aligned, 1) == 0) {
+          if(uip_ds6_nbr_update_lladdr(&nbr,
+                                   &lladdr_aligned) == 0) {
             /* failed to update the lladdr */
             goto discard;
           }
