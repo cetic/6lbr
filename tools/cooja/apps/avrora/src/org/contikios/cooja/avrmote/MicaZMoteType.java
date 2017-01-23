46336736e33d431a076464a86a418eb8225e8f1c
Laurent Deru <laurent.deru@cetic.be>
2017-01-23 14:07:28 +0100

    Revert "Fixed more MessageListUI to respect visualized in Cooja"
    This reverts commit a02481bc992841eadcb6fca68812a7ab72c9aa15.

--- tools/cooja/apps/avrora/src/org/contikios/cooja/avrmote/MicaZMoteType.java
+++ tools/cooja/apps/avrora/src/org/contikios/cooja/avrmote/MicaZMoteType.java
@@ -63,6 +63,8 @@ import org.contikios.cooja.avrmote.interfaces.MicaZLED;
 import org.contikios.cooja.avrmote.interfaces.MicaZRadio;
 import org.contikios.cooja.dialogs.CompileContiki;
 import org.contikios.cooja.dialogs.MessageList;
+import org.contikios.cooja.dialogs.MessageListText;
+import org.contikios.cooja.dialogs.MessageListUI;
 import org.contikios.cooja.dialogs.MessageContainer;
 import org.contikios.cooja.interfaces.Mote2MoteRelations;
 import org.contikios.cooja.interfaces.MoteAttributes;
@@ -364,7 +366,7 @@ public class MicaZMoteType implements MoteType {
       throw new MoteTypeCreationException("No identifier");
     }
 
-    final MessageList compilationOutput = MessageContainer.createMessageList(visAvailable);
+    final MessageList compilationOutput = visAvailable ? new MessageListUI() : new MessageListText();
 
     if (getCompileCommands() != null) {
       /* Handle multiple compilation commands one by one */
--- tools/cooja/apps/mspsim/src/org/contikios/cooja/mspmote/AbstractMspMoteType.java
+++ tools/cooja/apps/mspsim/src/org/contikios/cooja/mspmote/AbstractMspMoteType.java
@@ -118,7 +118,7 @@ public abstract class AbstractMspMoteType extends MspMoteType {
             throw new MoteTypeCreationException("No identifier");
         }
 
-        final MessageList compilationOutput = MessageContainer.createMessageList(visAvailable);
+        final MessageList compilationOutput = visAvailable ? new MessageListUI() : new MessageListText();
 
         if (getCompileCommands() != null) {
             /* Handle multiple compilation commands one by one */
