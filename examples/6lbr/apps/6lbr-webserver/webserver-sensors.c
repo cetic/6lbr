99f755b3bd24c132ae3c4e8e4db13a9b833ce743
Laurent Deru <laurent.deru@cetic.be>
2017-01-30 12:50:32 +0100

    Move node config web page out of node info

--- examples/6lbr/apps/6lbr-webserver/webserver-sensors.c
+++ examples/6lbr/apps/6lbr-webserver/webserver-sensors.c
@@ -232,49 +232,6 @@ PT_THREAD(generate_sensors_info(struct httpd_state *s))
   PSOCK_END(&s->sout);
 }
 
-#if CETIC_NODE_CONFIG
-static
-PT_THREAD(generate_sensors_config(struct httpd_state *s))
-{
-  PSOCK_BEGIN(&s->sout);
-  add("<br /><h2>Sensors configuration</h2>");
-  add
-    ("<table>"
-     "<theader><tr class=\"row_first\"><td>Node</td>");
-#if CETIC_NODE_CONFIG_HAS_NAME
-  add("<td>Name</td>");
-#endif
-  if((nvm_data.eth_ip64_flags & CETIC_6LBR_IP64_SPECIAL_PORTS) != 0) {
-    add("<td>CoAP</td><td>HTTP</td>");
-  }
-  add("</tr></theader><tbody>");
-  SEND_STRING(&s->sout, buf);
-  reset_buf();
-  if(node_config_loaded) {
-    static node_config_t *  node_config;
-    for(node_config = node_config_list_head(); node_config != NULL; node_config = list_item_next(node_config)) {
-      add("<tr><td>");
-      lladdr_add(&node_config->mac_address);
-      add("</td>");
-#if CETIC_NODE_CONFIG_HAS_NAME
-      add("<td>%s</td>", node_config_get_name(node_config));
-#endif
-      if((nvm_data.eth_ip64_flags & CETIC_6LBR_IP64_SPECIAL_PORTS) != 0) {
-        add("<td>%d</td><td>%d</td>", node_config->coap_port, node_config->http_port);
-      }
-      add("</tr>");
-      SEND_STRING(&s->sout, buf);
-      reset_buf();
-    }
-  }
-  add("</tbody></table><br />");
-  SEND_STRING(&s->sout, buf);
-  reset_buf();
-
-  PSOCK_END(&s->sout);
-}
-#endif
-
 static
 PT_THREAD(generate_sensors_tree(struct httpd_state *s))
 {
@@ -465,9 +422,6 @@ webserver_sensors_reset_stats_all(struct httpd_state *s)
   return &webserver_result_page;
 }
 HTTPD_CGI_CALL(webserver_sensors_info, "sensors.html", "Sensors", generate_sensors_info, 0);
-#if CETIC_NODE_CONFIG
-HTTPD_CGI_CALL(webserver_sensors_config, "sensors_config.html", "Sensors", generate_sensors_config, 0);
-#endif
 HTTPD_CGI_CALL(webserver_sensors_tree, "sensors_tree.html", "Node tree", generate_sensors_tree, 0);
 HTTPD_CGI_CALL(webserver_sensors_prr, "sensors_prr.html", "PRR", generate_sensors_prr, 0);
 HTTPD_CGI_CALL(webserver_sensors_ps, "sensors_ps.html", "Parent switch", generate_sensors_parent_switch, 0);
