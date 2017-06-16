c93755ceb9fd32deb4a8cb1e9d4098a884992071
Laurent Deru <laurent.deru@cetic.be>
2017-06-16 14:38:18 +0200

    Fix compilation error in no-framer

--- examples/ipv6/slip-radio/no-framer.c
+++ examples/ipv6/slip-radio/no-framer.c
@@ -55,13 +55,13 @@
  *   sending to.  If this value is 0xffff, the device is not
  *   associated.
  */
-static const uint16_t mac_dst_pan_id = IEEE802154_PANID;
+static uint16_t mac_dst_pan_id = IEEE802154_PANID;
 
 /**  \brief The 16-bit identifier of the PAN on which the device is
  *   operating.  If this value is 0xffff, the device is not
  *   associated.
  */
-static const uint16_t mac_src_pan_id = IEEE802154_PANID;
+static uint16_t mac_src_pan_id = IEEE802154_PANID;
 /*---------------------------------------------------------------------------*/
 /* Get current PAN ID */
 uint16_t
