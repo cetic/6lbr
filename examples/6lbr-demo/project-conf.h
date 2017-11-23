87faf610a4a42551c733f053653e9190195847a5
Laurent Deru <laurent.deru@cetic.be>
2017-11-23 22:19:08 +0100

    Add TSCH support in 6lbr-demo

--- examples/6lbr-demo/project-conf.h
+++ examples/6lbr-demo/project-conf.h
@@ -92,6 +92,73 @@
 /* Security                                                                  */
 /*---------------------------------------------------------------------------*/
 
+#if WITH_TSCH
+
+/* Netstack layers */
+#undef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC     tschmac_driver
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC     nordc_driver
+#undef NETSTACK_CONF_FRAMER
+#define NETSTACK_CONF_FRAMER  framer_802154
+
+/* IEEE802.15.4 frame version */
+#undef FRAME802154_CONF_VERSION
+#define FRAME802154_CONF_VERSION FRAME802154_IEEE802154E_2012
+
+/* TSCH and RPL callbacks */
+#define RPL_CALLBACK_PARENT_SWITCH tsch_rpl_callback_parent_switch
+#define RPL_CALLBACK_NEW_DIO_INTERVAL tsch_rpl_callback_new_dio_interval
+#define TSCH_CALLBACK_JOINING_NETWORK tsch_rpl_callback_joining_network
+#define TSCH_CALLBACK_LEAVING_NETWORK tsch_rpl_callback_leaving_network
+
+/* TSCH logging. 0: disabled. 1: basic log. 2: with delayed
+ * log messages from interrupt */
+#undef TSCH_LOG_CONF_LEVEL
+#define TSCH_LOG_CONF_LEVEL 2
+
+/* Start TSCH at init */
+#undef TSCH_CONF_AUTOSTART
+#define TSCH_CONF_AUTOSTART 1
+
+/* 6TiSCH minimal schedule length.
+ * Larger values result in less frequent active slots: reduces capacity and saves energy. */
+#undef TSCH_SCHEDULE_CONF_DEFAULT_LENGTH
+#define TSCH_SCHEDULE_CONF_DEFAULT_LENGTH 3
+
+/* Hardware configuration */
+
+/* Needed for CC2538 platforms only */
+/* For TSCH we have to use the more accurate crystal oscillator
+ * by default the RC oscillator is activated */
+#undef SYS_CTRL_CONF_OSC32K_USE_XTAL
+#define SYS_CTRL_CONF_OSC32K_USE_XTAL 1
+
+/* Needed for cc2420 platforms only */
+/* Disable DCO calibration (uses timerB) */
+#undef DCOSYNCH_CONF_ENABLED
+#define DCOSYNCH_CONF_ENABLED 0
+
+/* Enable SFD timestamps (uses timerB) */
+#undef CC2420_CONF_SFD_TIMESTAMPS
+#define CC2420_CONF_SFD_TIMESTAMPS 1
+
+#if CONTIKI_TARGET_CC2538DK || CONTIKI_TARGET_ZOUL || \
+  CONTIKI_TARGET_OPENMOTE_CC2538
+#define TSCH_CONF_HW_FRAME_FILTERING    0
+#endif /* CONTIKI_TARGET_CC2538DK || CONTIKI_TARGET_ZOUL \
+       || CONTIKI_TARGET_OPENMOTE_CC2538 */
+
+#if CONTIKI_TARGET_COOJA
+#define COOJA_CONF_SIMULATE_TURNAROUND 0
+#endif /* CONTIKI_TARGET_COOJA */
+
+#endif
+
+/*---------------------------------------------------------------------------*/
+/* Security                                                                  */
+/*---------------------------------------------------------------------------*/
+
 #ifndef WITH_ADAPTIVESEC
 #define WITH_ADAPTIVESEC 1
 #endif
