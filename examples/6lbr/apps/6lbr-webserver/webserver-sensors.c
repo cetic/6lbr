68a5abf78ea51a16d03f6e7bbb73787e7e136694
Pratyay Sarkar <104068902+pratyaynotfound@users.noreply.github.com>
2024-06-07 18:08:22 +0530

    update webserver-sensors.c

--- examples/6lbr/apps/6lbr-webserver/webserver-sensors.c
+++ examples/6lbr/apps/6lbr-webserver/webserver-sensors.c
@@ -242,44 +242,45 @@ PT_THREAD(generate_sensors_tree(struct httpd_state *s))
   add
     ("<center>"
      "<img src=\"https://quickchart.io/graphviz?graph=digraph%20%7B%20");
+
 #if CETIC_6LBR_NODE_CONFIG_HAS_NAME
-  node_config_t * my_config = node_config_find_by_lladdr(&uip_lladdr);
+  node_config_t *my_config = node_config_find_by_lladdr(&uip_lladdr);
   if (my_config) {
     add("%22%s%22;", node_config_get_name(my_config));
   } else {
     add("%22%04hx%22;",
-      (uip_lladdr.addr[6] << 8) + uip_lladdr.addr[7]);
+        (uip_lladdr.addr[6] << 8) + uip_lladdr.addr[7]);
   }
 #else
   add("%22%04hx%22;",
-    (uip_lladdr.addr[6] << 8) + uip_lladdr.addr[7]);
+      (uip_lladdr.addr[6] << 8) + uip_lladdr.addr[7]);
 #endif
 
-  for(i = 0; i < UIP_DS6_ROUTE_NB; i++) {
-    if(node_info_table[i].isused) {
-      if(!uip_is_addr_unspecified(&node_info_table[i].ip_parent)) {
+  for (i = 0; i < UIP_DS6_ROUTE_NB; i++) {
+    if (node_info_table[i].isused) {
+      if (!uip_is_addr_unspecified(&node_info_table[i].ip_parent)) {
 #if CETIC_6LBR_NODE_CONFIG_HAS_NAME
-        node_config_t * node_config = node_config_find_by_ip(&node_info_table[i].ipaddr);
-        node_config_t * parent_node_config = node_config_find_by_ip(&node_info_table[i].ip_parent);
+        node_config_t *node_config = node_config_find_by_ip(&node_info_table[i].ipaddr);
+        node_config_t *parent_node_config = node_config_find_by_ip(&node_info_table[i].ip_parent);
         if (node_config) {
           if (parent_node_config) {
-            add("%22%s%22->%22%s%22;",
+            add("%22%s%22-%3E%22%s%22;",
                 node_config_get_name(node_config),
                 node_config_get_name(parent_node_config));
           } else {
-            add("%22%s%22->%22%04hx%22;",
+            add("%22%s%22-%3E%22%04hx%22;",
                 node_config_get_name(node_config),
                 (node_info_table[i].ip_parent.u8[14] << 8) +
                 node_info_table[i].ip_parent.u8[15]);
           }
         } else {
           if (parent_node_config) {
-            add("%22%04hx%22->%22%s%22;",
+            add("%22%04hx%22-%3E%22%s%22;",
                 (node_info_table[i].ipaddr.u8[14] << 8) +
                 node_info_table[i].ipaddr.u8[15],
                 node_config_get_name(parent_node_config));
           } else {
-            add("%22%04hx%22->%22%04hx%22;",
+            add("%22%04hx%22-%3E%22%04hx%22;",
                 (node_info_table[i].ipaddr.u8[14] << 8) +
                 node_info_table[i].ipaddr.u8[15],
                 (node_info_table[i].ip_parent.u8[14] << 8) +
@@ -287,7 +288,7 @@ PT_THREAD(generate_sensors_tree(struct httpd_state *s))
           }
         }
 #else
-        add("%22%04hx%22->%22%04hx%22;",
+        add("%22%04hx%22-%3E%22%04hx%22;",
             (node_info_table[i].ipaddr.u8[14] << 8) +
             node_info_table[i].ipaddr.u8[15],
             (node_info_table[i].ip_parent.u8[14] << 8) +
@@ -298,7 +299,8 @@ PT_THREAD(generate_sensors_tree(struct httpd_state *s))
       }
     }
   }
-  add("}\" alt=\"Sensor Tree\" /></center>");
+
+  add("%7D\" alt=\"Sensor Tree\" /></center>");
   SEND_STRING(&s->sout, buf);
   reset_buf();
 
